deb8837304fce0c8c5b7fca4031f532e2b4fca5e
Milagrosbriano <127800403+Milagrosbriano@users.noreply.github.com>
2023-05-28 17:55:12 -0300

    Create Shield.cs

---
+++ src/Library/items/Shield.cs
@@ -0,0 +1,13 @@
+namespace RoleplayGame.Items
+{
+    public class Shield: IDefenseItem
+    {
+        public int DefensePower
+        {
+            get
+            {
+                return 14;
+            }
+        }
+    }
+}
